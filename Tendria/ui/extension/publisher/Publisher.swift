6a454d3cc43af9572de643f8dd5e30d1f5db4b6f
Yakup Kavak <yakupkavk@gmail.com>
2025-03-26 16:23:23 +0300

    feat: add keyboard adaptive

---
+++ Tendria/ui/extension/publisher/Publisher.swift
@@ -0,0 +1,8 @@
+//
+//  Publisher.swift
+//  Tendria
+//
+//  Created by Yakup Kavak on 26.03.2025.
+//
+
+import Foundation
---
+++ Tendria/ui/extension/viewDesign/ExtensionUI.swift
@@ -0,0 +1,31 @@
+//
+//  ExtensionUI.swift
+//  Tendria
+//
+//  Created by Yakup Kavak on 20.01.2025.
+//
+
+import Foundation
+import SwiftUI
+typealias Padding = Constants.Padding
+extension View {
+    func normalShadow() -> some View {
+        self.shadow(color: Color.black.opacity(0.2), radius: Constants.Radius.shadowRadius, x: 0, y: 5)
+    }
+    func gradientBackground() -> some View{
+        self.background(LinearGradient(
+            gradient: Gradient(colors: [
+                Color.orange700, Color.red300]),
+            startPoint: .leading,
+            endPoint: .trailing
+        ))
+    }
+    func customPadding(horizontal: CGFloat, vertical: CGFloat) -> some View {
+        self
+            .padding(.horizontal, horizontal)
+            .padding(.vertical, vertical)
+    }
+    func btnStyle() -> some View{
+        self.customPadding(horizontal: Padding.mediumPadding, vertical: Padding.smallPadding).gradientBackground().normalShadow().cornerRadius(Constants.Radius.xLargeRadius)
+    }
+}
