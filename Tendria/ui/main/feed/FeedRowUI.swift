1493149ac6ef8a319270a6f7028f3fd9ecb04e6e
Yakup Kavak <yakupkavk@gmail.com>
2025-07-30 21:38:50 +0300

    feat: add questions ui

---
+++ Tendria/ui/main/feed/FeedRowUI.swift
@@ -0,0 +1,18 @@
+//
+//  FeedRowUI.swift
+//  Tendria
+//
+//  Created by Yakup Kavak on 15.07.2025.
+//
+
+import SwiftUI
+
+struct FeedRowUI: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FeedRowUI()
+}
